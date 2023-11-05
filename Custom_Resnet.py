2bd644476ee07b96c75c3fd054fe3ceff9d15c21
tnugd0 <94063417+tnugd0@users.noreply.github.com>
2023-11-05 18:59:49 +0700

    Update Custom_Resnet.py

--- Custom_Resnet.py
+++ Custom_Resnet.py
@@ -7,28 +7,36 @@ class Custom_Resnet50(nn.Module):
     def __init__(self, num_class = 43):
         super().__init__()
         self.model = resnet50( weights  = ResNet50_Weights.IMAGENET1K_V2)
+        # print(self.model)
         del self.model.fc
         for name , param in self.model.named_parameters():
             param.requires_grad = False
-            
+
 
         for param in self.model.layer4.parameters():
             param.requires_grad = True
 
+
+        for param in self.model.layer3.parameters():
+            param.requires_grad = True
+
+        for param in self.model.layer2.parameters():
+            param.requires_grad = True
+
         self.fc1 = nn.Sequential(
-            nn.Dropout(p=0.6),
-            nn.Linear(in_features=1024, out_features=1024),
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features=2048, out_features=1024),
             nn.LeakyReLU()
         )
 
         self.fc2 = nn.Sequential(
-            nn.Dropout(p=0.6),
+            nn.Dropout(p=0.5),
             nn.Linear(in_features = 1024, out_features=512),
             nn.LeakyReLU()
         )
 
         self.fc3 = nn.Sequential(
-            nn.Dropout(p=0.6),
+            nn.Dropout(p=0.5),
             nn.Linear(in_features = 512, out_features=num_class),
         )
 
@@ -45,7 +53,7 @@ class Custom_Resnet50(nn.Module):
         x = self.model.layer4(x)
 
         x = self.model.avgpool(x)
-        x = torch.flatten(x, 1)
+        x = torch.flatten(x,1)
         x = self.fc1(x)
         x = self.fc2(x)
         x = self.fc3(x)
@@ -56,6 +64,6 @@ class Custom_Resnet50(nn.Module):
         return self._forward_impl(x)
 
 
-my_retnet = Custom_Resnet50()
-print(my_retnet.model)
+# my_retnet = Custom_Resnet50()
+# print(my_retnet.model)
 # print(self.model)
